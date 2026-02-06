2ee4305a9d64eb161cb80b08ee9605bd4a3a42f9
gwontak.seo <gwontak.seo@lge.com>
2026-02-06 16:54:43 +0900

    step12. Movie App

---
+++ step12/src/components/Movies.js
@@ -0,0 +1,16 @@
+function Movies ({coverImg, title, summary, genres }) {
+    return (
+        <div>
+            <img src={coverImg} alt={title} />
+            <h2>{title}</h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Movies;
